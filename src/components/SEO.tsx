41be3475f3ac05e857c884693356b5133431e391
andreimoraru-rgb <andrei.moraru@ingbroker.md>
2026-05-21 11:59:06 +0300

    SEO fix: SEO.tsx cleanup attempt - fix duplicate closing tags

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -70,7 +70,6 @@ export const SEO = ({ title, description, lang, schema, keywords, image = "https
                           </script>
                     )}
               </Helmet>
+                );
               );
-              );
-                    };
-};
+                    
